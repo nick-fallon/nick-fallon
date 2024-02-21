c18c26391c5610551ab6c1133ece308f3773743d
Nick Fallon <nicholas.p.fallon@gmail.com>
2024-02-21 00:24:30 -0800

    feat(routing): add very basic routing

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import About from "@/components/about/About";
+
+export default function about() {
+  return (
+    <main>
+      <div className="p-4">
+        <h1>About Page</h1>
+        <About />
+      </div>
+    </main>
+  )
+}
